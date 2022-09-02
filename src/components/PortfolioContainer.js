4e8be4fc9d8eb4d1353c5d0b806d95b8d20ca6c0
mio kataoka <mio.kanna@gmail.com>
2022-09-02 14:28:42 -0700

    commit tree 92c7cc6fb905415d819a14c9535811366d5812d6

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({portfolio, handleRemoveClick}) {
+  function handleTileClick(stock){
+    handleRemoveClick(stock)
+  }
+  let myPortfolio = portfolio.map(stock => (
+    <Stock key={stock.id} stock={stock} onClick={handleTileClick} />
+  ))
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      {myPortfolio}
     </div>
   );
 }
